1e1e7c4a4c0aa8eaa5c88ee2f732028b547b998e
Rockefeller <kenny71223@gmail.com>
2025-09-16 18:33:54 +0800

    🎉 init: init again with battle game book sample

---
+++ scripts/Move.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name Move
+
+var id: String
+var name: String
+var damage: int = 0
+var defense: int = 0
+var special: Dictionary = {}   # e.g. {"break_armor": true}
--- scripts/Player.gd
+++
@@ -1,8 +0,0 @@
-# Player.gd
-extends Resource
-class_name Player
-
-var name = "Hero"
-var hp = 20
-var attack = 5
-var defense = 2
